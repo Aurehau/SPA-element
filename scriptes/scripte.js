3c78456aed7d70b56e1c0229ec7f8e0e233e2e76
Aurehau <114416513+Aurehau@users.noreply.github.com>
2024-08-04 10:54:31 +0200

    a

--- scriptes/scripte.js
+++ scriptes/scripte.js
@@ -97,7 +97,7 @@ if (document.querySelector("main>div").className === "card modif_compte") {
 }
 
 var map = L.map('map').setView([51.505, -0.09], 13);
-L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	minZoom: 0,
 	maxZoom: 20,
 	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
